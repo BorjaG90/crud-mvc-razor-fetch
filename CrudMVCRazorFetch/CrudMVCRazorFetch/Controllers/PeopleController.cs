447c5688a144d5528dff7ab111bba0a135954c4d
Borja Gete <borjag90dev@gmail.com>
2020-10-07 15:04:21 +0200

    feat: Basic single page list of people

---
+++ CrudMVCRazorFetch/CrudMVCRazorFetch/Controllers/PeopleController.cs
@@ -0,0 +1,37 @@
+﻿using CrudMVCRazorFetch.Models;
+using CrudMVCRazorFetch.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudMVCRazorFetch.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+				{
+            List<ListPeopleViewModel> lst = new List<ListPeopleViewModel>();
+            using (CrudMVCRazorFetchEntities db= new CrudMVCRazorFetchEntities()) 
+            {
+                lst = 
+                    (from d in db.people
+                    orderby d.id descending
+                    select new ListPeopleViewModel
+										{
+                        Id=d.id,
+                        Name=d.name,
+                        Age=d.age
+										}).ToList();
+            }
+            return View(lst);
+				}
+    }
+}
